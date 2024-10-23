055055b82eb0288c2f88d16c62c0012e5bc82f5b
Ryo Suzuki <astronautryosuzuki@gmail.com>
2024-10-23 21:54:46 +0900

    1st commit

---
+++ tagmemo/src/components/HeaderView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>TagMemo</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+h2 {
+  font-size: 2.4rem;
+  margin: 0;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h2,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ tagmemo/src/components/WelcomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconLogin.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Login</template>
+    このサービスを利用するにはログインが必要です。
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Setting</template>
+    アカウントの設定を行います。(ログインが必要です)
+  </WelcomeItem>
+</template>
